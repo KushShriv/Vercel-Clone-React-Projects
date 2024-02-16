f927e98f8591374db1e2e9424a3590e1fd72e8a0
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2024-02-17 00:05:27 +0530

    Started Writing Upload Service

---
+++ vercel-upload-service/src/index.ts
@@ -0,0 +1,44 @@
+
+import express from "express";
+import cors from "cors";
+import simpleGit from "simple-git";
+import { generate } from "./utils";
+import { getAllFiles } from "./file";
+import path from "path";
+import { uploadFile } from "./aws";
+import { createClient } from "redis";
+
+const publisher = createClient();
+publisher.connect();
+const subscriber = createClient();
+subscriber.connect();
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+app.post("/deploy", async (req, res) => {
+    const repoUrl = req.body.repoUrl;
+    const id = generate(); // ay3r4
+    await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
+
+    const files = getAllFiles(path.join(__dirname, `output/${id}`));
+
+    files.forEach(async file => {
+        await uploadFile(file.slice(__dirname.length + 1), file);
+    })
+
+    
+    await new Promise((resolve) => setTimeout(resolve, 5000))
+    // Push to build queue
+    publisher.lPush("build-queue", id);
+    // Set the status of the id to uploaded
+    publisher.hSet("status", id, "uploaded");
+
+    res.json({
+        id: id
+    })
+
+});
+
+app.listen(3000);
