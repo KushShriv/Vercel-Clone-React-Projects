0a63822b321879068976435dbb3477855cc02d38
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2024-02-20 10:06:18 +0530

    Added Redis and AWS Push to Upload Service

---
+++ vercel-upload-service/src/aws.ts
@@ -0,0 +1,21 @@
+import { S3 } from "aws-sdk";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
+const s3 = new S3({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  endpoint: process.env.S3_ENDPOINT,
+});
+
+export const uploadFile = async (fileName: string, localFilePath: string) => {
+    const fileContent = fs.readFileSync(localFilePath);
+    const response = await s3.upload({
+        Body: fileContent,
+        Bucket: "vercel",
+        Key: fileName,
+    }).promise();
+    console.log(response);
+}
