207abbc68d24947e35701ef5e45af1b94d760651
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2024-02-28 20:29:47 +0530

    Completed Request Handler Service

---
+++ vercel-request-handler/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import { S3 } from "aws-sdk";
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
+const s3 = new S3({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  endpoint: process.env.S3_ENDPOINT,
+});
+
+
+const app = express();
+
+app.get("/*", async (req, res) => {
+    const host = req.hostname;
+
+    const id = host.split(".")[0];
+    const filePath = req.path;
+
+    const contents = await s3.getObject({
+        Bucket: "vercel",
+        Key: `dist/${id}${filePath}`
+    }).promise();
+    
+    const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript"
+    res.set("Content-Type", type);
+
+    res.send(contents.Body);
+})
+
+app.listen(3001);
